acb2015cd043bc24cd84fae831219143ff37bef0
Fabio Stohler <58552478+Fabio-Stohler@users.noreply.github.com>
2021-07-08 15:51:10 +0200

    Update

--- Functions/EGM.jl
+++ Functions/EGM.jl
@@ -18,9 +18,9 @@ Returns
 """
 function EGM(C,mutil,invmutil,par,mpar,Π,meshes,gri)
     mu     = mutil(C) # Calculate marginal utility from c'
-    emu    = mu*Π'    # Calculate expected marginal utility
-    Cstar  = invmutil((1+par.r)*par.β.*emu)     # Calculate cstar(m',z)
-    Kstar  = (meshes.k  .+ Cstar .- meshes.z)/(1+par.r) # Calculate mstar(m',z)
+    emu    = mu*Π'     # Calculate expected marginal utility
+    Cstar  = invmutil(par.β *(1+par.r) * emu)     # Calculate cstar(m',z)
+    Kstar  = (Cstar  + meshes.k - meshes.z)/(1+par.r) # Calculate mstar(m',z)
     Kprime = copy(meshes.k) # initialize Capital Policy
 
     for z=1:mpar.nz # For massive problems, this can be done in parallel
@@ -28,25 +28,24 @@ function EGM(C,mutil,invmutil,par,mpar,Π,meshes,gri)
         Savings     = CubicSpline(sort(Kstar[:,z]),gri.k[sortperm(Kstar[:,z])])
         # Implement linear extrapolation
         mink = minimum(Kstar[:,z]); maxk = maximum(Kstar[:,z])
-        minSlope = (Savings[mink+0.01]-Savings[mink])/0.01  # Numerical derivative at mink
-        maxSlope = (Savings[maxk]-Savings[maxk-0.01])/0.01  # Numerical derivative at maxk
+        minSlope = (Savings[mink+0.01]-Savings[mink])/0.01
+        maxSlope = (Savings[maxk]-Savings[maxk-0.01])/0.01
         function SavingsExtr(k)
-            # Function that, given k, gives back savings. Given k is outside the grid, we extrapolate the functional values
             if mink <= k <= maxk
-                return Savings[k] 
+                return Savings[k]
             elseif k < mink
-                return Savings[mink]+minSlope*(k-mink)  # Extrapolating
+                return Savings[mink]+minSlope*(k-mink)
             else
-                return Savings[maxk]+maxSlope*(k-maxk)  # Extrapolating
+                return Savings[maxk]+maxSlope*(k-maxk)
             end
         end 
         Kprime[:,z] = SavingsExtr.(gri.k)   # Obtain k'(z,k) by interpolation
-        BC          = gri.k .< Kstar[1,z]   # Check Borrowing Constraint
+        BC          = gri.k .< Kstar[1,z] # Check Borrowing Constraint
         # Replace Savings for HH saving at BC
-        Kprime[BC,z].= mpar.mink # Households with the BC flag choose borrowing contraint
+        Kprime[BC,z].= par.b # Households with the BC flag choose borrowing contraint
     end
     # generate consumption function c(z,k^*(z,k'))
-    C          = meshes.z + (1+par.r)*meshes.k - Kprime # Consumption update (not using Kstar, since this is the old policy function)
+    C          = meshes.k*(1+par.r) + meshes.z - Kprime #Consumption update
     return C, Kprime
 end
 
@@ -68,22 +67,22 @@ Returns
     * `Kprime`: (k x z) asset policy
 """
 function EGM(C,mutil,invmutil,R,RPrime,par,mpar,Π,meshes,gri)
-    C      = reshape(xxx,mpar.nk,mpar.nz)
-    mu     = mutil(xxx) # Calculate marginal utility from c'
-    emu    = xxx     # Calculate expected marginal utility
-    Cstar  = invmutil(xxx)     # Calculate cstar(m',z)
-    Kstar  = (xxx  + xxx - xxx)/RPrime # Calculate mstar(m',z)
-    Kprime = ones(eltype(xxx),size(meshes.k)) # initialize Capital Policy
+    C      = reshape(C,mpar.nk,mpar.nz)
+    mu     = mutil(C) # Calculate marginal utility from c'
+    emu    = mu*Π'     # Calculate expected marginal utility
+    Cstar  = invmutil(par.β *RPrime * emu)     # Calculate cstar(m',z)
+    Kstar  = (Cstar  + meshes.k - meshes.z)/RPrime # Calculate mstar(m',z)
+    Kprime = ones(eltype(C),size(meshes.k)) # initialize Capital Policy
 
     for z=1:mpar.nz # For massive problems, this can be done in parallel
         # generate savings function k(z,kstar(k',z))=k'
-        Kprime[:,z]  = mylinearinterpolate(sort(xxx),gri.k[sortperm(xxx)],gri.k) # Obtain k'(z,k) by interpolation
-        BC          = xxx .< xxx # Check Borrowing Constraint
+        Kprime[:,z]  = mylinearinterpolate(sort(Kstar[:,z]),gri.k[sortperm(Kstar[:,z])],gri.k) # Obtain k'(z,k) by interpolation
+        BC          = gri.k .< Kstar[1,z] # Check Borrowing Constraint
         # Replace Savings for HH saving at BC
-        Kprime[BC,z].= xxx # Households with the BC flag choose borrowing contraint
+        Kprime[BC,z].= par.b # Households with the BC flag choose borrowing contraint
     end
     # generate consumption function c(z,k^*(z,k'))
-    C          = xxx + xxx - xxx #Consumption update
+    C          = meshes.k*R + meshes.z - Kprime #Consumption update
     return C, Kprime
 end
     
--- Functions/TransitionMat.jl
+++ Functions/TransitionMat.jl
@@ -1,7 +1,7 @@
 using LinearAlgebra
 
 """
-    TransitionMat(kprime,gri,mpar,Π)
+    Young(kprime,gri,mpar,Π)
 
 Calculate a transition matrix from policy functions
 and use this to obtain the stationary distribution of
@@ -15,6 +15,7 @@ income and wealth (Young's method)
 Returns
 
     * `Γ`: transition matrix from policy functions
+    * `StDist`: stationary joint distribution (k x z)
 """
 function TransitionMat(kprime,gri,mpar,Π)
     idk                  = [searchsortedlast(gri.k,k) for k in kprime[:]] # find the next lowest point on grid for policy
@@ -31,8 +32,8 @@ function TransitionMat(kprime,gri,mpar,Π)
     # Fill this array with probabilities
     for zz=1:mpar.nz # all current income states
         for kk=1:mpar.nk # all current asset states
-            Trans_array[kk,zz,idk[kk,zz],:]   =  weightleft[kk,zz]*reshape(Π[zz,:],1,1,1,mpar.nz) # probability to move left  to optimal policy choice (only function of the starting points)
-            Trans_array[kk,zz,idk[kk,zz]+1,:] =  weightright[kk,zz]*reshape(Π[zz,:],1,1,1,mpar.nz) # probability to move right to optimal policy choice (only function of the starting points)
+            Trans_array[kk,zz,idk[kk,zz],:]   =  weightleft[kk,zz] *reshape(Π[zz,:],1,1,1,mpar.nz) # probability to move left  to optimal policy choice
+            Trans_array[kk,zz,idk[kk,zz]+1,:] =  weightright[kk,zz] *reshape(Π[zz,:],1,1,1,mpar.nz) # probability to move right to optimal policy choice
         end
     end
     Γ  = reshape(Trans_array,mpar.nk*mpar.nz, mpar.nk*mpar.nz) # Turn 4-d array into a transition matrix stacking 2 dimensions
